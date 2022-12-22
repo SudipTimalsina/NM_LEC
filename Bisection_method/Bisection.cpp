712c8c6f3f4ebf75f5fe263ed00be066e11669b0
SudipTimalsina <timalsinasudip11@gmail.com>
2022-12-22 20:17:45 +0545

    Bisection

---
+++ Bisection_method/Bisection.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+
+using namespace std;
+
+float fun(float x){
+    return (x*x*x + 3*x*x - 5);
+}
+
+int main(){
+    float a,b,e,mid;
+    cout<<"Enter the starting interval: "<<endl;
+    cin>>a;
+    cout<<"Enter the final interval: "<<endl;
+    cin>>b;
+    cout<<"What is the tolerable error? : "<<endl;
+    cin>>e;
+    cout<<endl;
+    int n = ceil((log10(b-a)-log10(e))/log10(2));
+    cout<<"The minimum number of iterations required are : "<<n<<endl<<endl;
+    int count=0;
+    cout<<setw(5)<<"i"<<setw(10)<<"a"<<setw(10)<<"b"<<setw(10)<<"mid"<<setw(10)<<"fmid"<<setw(10)<<endl;
+    cout<<"----------------------------------------------------"<<endl<<endl;
+    do{
+        mid = (a+b)/2;
+        if (fun(a)*fun(mid) > 0)
+        {
+            a = mid;
+        }
+        else{
+            b = mid;
+        }
+        count++;
+        cout<<setw(5)<<count<<setw(10)<<a<<setw(10)<<b<<setw(10)<<mid<<setw(15)<<fun(mid)<<setw(10)<<endl;
+        
+    }while((abs(fun(mid)) > e && count <= (2*n)));
+    return 0;
+}
