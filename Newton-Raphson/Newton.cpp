9c82e3e30dad74607b1b5257b9c7e90772e3d055
SudipTimalsina <timalsinasudip11@gmail.com>
2022-12-25 22:25:48 +0545

    Newton-Raphson

---
+++ Newton-Raphson/Newton.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+float calc(float x)
+{
+    return (x*log10(x)-1.2);
+}
+float dcalc(float x)
+{
+    return (log10(x)+0.43429);
+}
+
+int main()
+    {
+        float a, b, c, e;
+        cout<<"Enter the interval (a,b) between whicn the ans is needed"<<endl;
+        cin>>a>>b;
+        cout<<"Enter the toloratable error "<<endl;
+        cin>>e;
+        int count=0;
+        float x0=((a+b)/2);
+
+        do {
+            count++;
+            if(abs(dcalc(x0))<0.000005)
+            {
+                cout<<"Math Error"<<endl;
+                return 1; 
+            }
+            if (count>50)
+                {cout<<"Time Out Error";
+                 return 2;
+                }
+            else 
+            {
+                c=x0-(calc(x0)/dcalc(x0));
+                x0=c;
+            }
+        }while(abs(calc(c)>e));
+        cout<<"ans="<<c<<endl;
+        return 0;
+    }
